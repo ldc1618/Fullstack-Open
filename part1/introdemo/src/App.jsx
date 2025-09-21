0dce2cb997a7551fa7c3202fcfd3316df34c9f3e
Luke Chelius <ldc1618@rit.edu>
2025-09-20 23:19:02 -0400

    Messing around with part c code

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,33 +1,36 @@
-const Hello = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
+import { useState } from 'react'
 
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href='https://github.com/ldc1618'>ldc1618</a>
-    </div>
-  )
-}
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
   return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name='Maya' age={26 + 10}/>
-      <Hello name={name} age={age} />
-      <Footer />
-    </>
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
+    </div>
   )
-}
+} 
 
 export default App
