d6355982a5a31e7eca127caced5e2194555ce281
Luke Chelius <ldc1618@rit.edu>
2025-09-22 00:47:52 -0400

    Working through 1d code

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,36 +1,29 @@
 import { useState } from 'react'
 
-const Display = ({ counter }) => <div>{counter}</div>
+const Display = props => <div>{props.value}</div>
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
-  console.log('rendering with counter value', counter)
+  const [value, setValue] = useState(10)
 
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
-
-  const decreaseByOne = () => { 
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
+  const setToValue = newValue => {
+    console.log('value now', newValue)
+    setValue(newValue)
   }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      <Display value={value} />
+      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <Button onClick={() => setToValue(0)} text="reset" />
+      <Button onClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
-} 
+}
 
 export default App
