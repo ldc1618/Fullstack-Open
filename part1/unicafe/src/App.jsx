046be3fb1df7de5f06cad6345b3a98c8e5496cbb
Luke Chelius <ldc1618@rit.edu>
2025-09-24 00:46:37 -0400

    Completed exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,10 +11,30 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
-  const handleClickGood = () => setGood(good + 1)
-  const handleClickNeutral = () => setNeutral(neutral + 1)
-  const handleClickBad = () => setBad(bad + 1)
+  const handleClickGood = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+    setAverage(((good + 1) + (bad * -1))/(all + 1))
+    setPositive((good + 1)/(all + 1) * 100)
+  }
+
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+    setAverage((good + (bad * -1))/(all + 1))
+    setPositive(good/(all + 1) * 100)
+  }
+
+  const handleClickBad = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+    setAverage((good + ((bad + 1) * -1))/(all + 1))
+    setPositive(good/(all + 1) * 100)
+  }
 
   return (
     <div>
@@ -26,9 +46,12 @@ const App = () => {
       </div>
       <h1>statistics</h1>
       <div>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
+        <div>good {good}</div>
+        <div>neutral {neutral}</div>
+        <div>bad {bad}</div>
+        <div>all {all}</div>
+        <div>average {average}</div>
+        <div>positive {positive} %</div>
       </div>
     </div>
   )
