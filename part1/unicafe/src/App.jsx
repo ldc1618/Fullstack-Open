9239447bd985e670d88c324fb643a21c24d1b985
Luke Chelius <ldc1618@rit.edu>
2025-09-24 01:11:36 -0400

    Completed exercise 1.8 and did some refactoring

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,35 +6,29 @@ const Button = ({ onClick, text }) => {
   )
 }
 
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = all ? (good - bad) / all : 0
+  const positive = all ? (good / all) * 100 : 0
+
+  return (
+    <>
+      <div>all {all}</div>
+      <div>average {average}</div>
+      <div>positive {positive} %</div>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
 
-  const handleClickGood = () => {
-    setGood(good + 1)
-    setAll(all + 1)
-    setAverage(((good + 1) + (bad * -1))/(all + 1))
-    setPositive((good + 1)/(all + 1) * 100)
-  }
-
-  const handleClickNeutral = () => {
-    setNeutral(neutral + 1)
-    setAll(all + 1)
-    setAverage((good + (bad * -1))/(all + 1))
-    setPositive(good/(all + 1) * 100)
-  }
-
-  const handleClickBad = () => {
-    setBad(bad + 1)
-    setAll(all + 1)
-    setAverage((good + ((bad + 1) * -1))/(all + 1))
-    setPositive(good/(all + 1) * 100)
-  }
+  const handleClickGood = () => setGood(good + 1)
+  const handleClickNeutral = () => setNeutral(neutral + 1)
+  const handleClickBad = () => setBad(bad + 1)
 
   return (
     <div>
@@ -49,9 +43,7 @@ const App = () => {
         <div>good {good}</div>
         <div>neutral {neutral}</div>
         <div>bad {bad}</div>
-        <div>all {all}</div>
-        <div>average {average}</div>
-        <div>positive {positive} %</div>
+        <Statistics good={good} neutral={neutral} bad={bad} />
       </div>
     </div>
   )
